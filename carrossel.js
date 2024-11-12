125d07eb798d75859c1ca87124e1e0b60dec34ba
marcosdaniel777 <dansza777@gmail.com>
2024-11-12 16:32:37 -0300

    ak

--- carrossel.js
+++ carrossel.js
@@ -1,1 +1,23 @@
+let currentSlide = 0;
 
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-image');
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function nextSlide() {
+    const slides = document.querySelectorAll('.carousel-image');
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+function prevSlide() {
+    const slides = document.querySelectorAll('.carousel-image');
+    currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+    showSlide(currentSlide);
+}
